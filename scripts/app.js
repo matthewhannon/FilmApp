64fa544150f69771d7193047d419f360bca4e331
unknown <matthannon88@gmail.com>
2020-04-07 09:45:27 -0700

    Added TMDb API, CSS and it now shows the director and list of 5 actors

--- scripts/app.js
+++ scripts/app.js
@@ -1,53 +1,77 @@
-console.log('app.js test');
+// DOM EVENTS
+const titleInput = document.querySelector('.title-search');
+const titleButton = document.querySelector('button');
+const searchDisplay = document.querySelector('.search-display');
+const showPoster = document.querySelector('.poster');
+const actorList = document.querySelector('.actors');
+const director = document.querySelector('.director').childNodes;
 
-const titleSearch = document.querySelector('.title-search');
-const yearSearch = document.querySelector('.year-search');
-const buttonClick = document.querySelector('button');
-
-const title = document.querySelector('.title');
-const year = document.querySelector('.year');
-const rating = document.querySelector('.rating');
-const img = document.querySelector('img');
-
-buttonClick.addEventListener('click', e => {
-
-    e.preventDefault();
-    
-    // console.log('test');
-    // console.log(titleSearch.value)
-    // console.log(yearSearch.value)
-
-    getMovie(titleSearch.value, yearSearch.value)
-    .then(data => {
-        console.log(data);
-        title.innerHTML = (`This movie is titled ${data.Title}`);
-        year.innerHTML = (`This movie was released in ${data.Released}`);
-        rating.innerHTML = (`It is Rated ${data.Rated}`);
-        updatePoster(data.Poster);
-    })
-    .catch(err => console.log(err))
+//Pressing Enter Key
+titleInput.addEventListener("keyup", e => {
+    if(event.keyCode === 13){
+        //cancel the default action, if needed
+        event.preventDefault();
+        //'clicks' button for us
+        titleButton.click();
+        titleInput.value="";
+    }
 });
 
-const updatePoster = (poster) => {
-    if(poster == 'N/A'){
-        img.style="display: none";
-    }else{
-        img.src = `${poster}`;
+//Clicking Search Button
+titleButton.addEventListener('click',(e) => {
+    const titleSearch = `&query=${titleInput.value}`;
+
+    getData(titleSearch).then((data)=>{
+        console.log(data[0]); //first data
+        console.log(data[1]); //data details
+        console.log(data[2]); //credits
+
+        const title = data[0].original_title;
+        const posterLink = data[1].poster_path;
+        const actorCredits = data[2];
+        const crew = data[2].crew;
+        
+        searchTitleDisplay(title);
+        showImage(posterLink);
+        updateActors(actorCredits);
+        showDirector(crew)
+    });
+});
+
+//Displays Search Title
+const searchTitleDisplay = (title) => {
+    searchDisplay.innerHTML= `${title}`;
+    searchDisplay.style.visibility= "visible";
+};
+
+//Shows film poster
+const showImage = (link) => {
+    const imageLink = `http://image.tmdb.org/t/p/w300/${link}`
+    showPoster.src = `${imageLink}`;
+};
+
+const updateActors = (actorsArray) => {
+    const actors = actorsArray.cast;
+    console.log(actors);
+
+    const actorsText = actorList.childNodes[1].children;
+    // console.log(actorsText[0])
+    // console.log(actors[0].name)
+
+    // console.log(actors.length);
+
+    for(var i = 0; i < 5; i++){
+        let image = `http://image.tmdb.org/t/p/w200/${actors[i].profile_path}`;
+        actorsText[i].innerHTML = `<img class="actor-img" src=${image}><p>${actors[i].name} as ${actors[i].character}</p>`;
     }
 }
 
-
-/*
-    search top 10 films by length given in input field
-    search by genre
-    sort by rating
-    sort by awards?
-
-*/
-
-
-
-
-
-
-
+const showDirector = (crewArray) => {
+    for(var i = 0; i < crewArray.length; i++){
+        if(crewArray[i].job === "Director"){
+            console.log(i, crewArray[i].name);
+            director[1].innerHTML = "Directed By"
+            director[3].innerHTML = `${crewArray[i].name}`;
+        }
+    }
+}
--- scripts/movieData.js
+++ scripts/movieData.js
@@ -1,12 +1,29 @@
+//API DATA
 const key = config.MY_KEY;
 
-const getMovie = async (title, year) => {
-    const searchQ = `?t=${title}&y=${year}`;
-    const baseURL = `http://www.omdbapi.com/${searchQ}&apikey=${key}&`;
-    
-    // console.log(baseURL);
-    const response = await fetch(baseURL);
+const getData = async (title) => {
+    //Broad Search
+    const source = `https://api.themoviedb.org/3/search/movie?api_key=${key}`;
+    const response = await fetch(source+title);
     const data = await response.json();
 
-    return data;
+    //less details data
+    const firstResult = data.results[0];
+    // console.log(firstResult);
+
+    //Search after getting ID
+    const idNum = data.results[0].id;
+
+    //Full Details
+    const sourceID = `https://api.themoviedb.org/3/movie/${idNum}?api_key=${key}&append_to_response=credits`;
+    const getDetails = await fetch(sourceID);
+    const dataDetails = await getDetails.json();
+    const credits = dataDetails.credits;
+
+    return [firstResult, dataDetails, credits];
+
+
+    //notes
+    // I could add this to end of the link if I want links to video
+    // const videos = `&append_to_response=videos`;
 }
